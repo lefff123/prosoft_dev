7e3d4632ac4aef02ac36626f0ea16d3db8bd025d
lefff123 <lev.king.2035@gmail.com>
2025-09-27 11:15:41 +0500

    Update mime_type.c

--- mime_type.c
+++ mime_type.c
@@ -76,7 +76,7 @@ int main()
 
 
     // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
-    
+    free(mime);
 
     return 0;
 }
